c924b997a706745dbfe80815a2c15145e756db92
johnolivercr <j.quesada.chaves@gmail.com>
2023-03-20 20:21:46 -0600

    matrices

--- src/App.java
+++ src/App.java
@@ -3,6 +3,6 @@ import BusinessLogic.Menu;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        new CashierJFrame();
+        //new CashierJFrame();
     }
 }
---
+++ src/Tablero.java
@@ -0,0 +1,27 @@
+
+public class Tablero {// inicio de la clase
+
+    public static void main(String[] args) {// metodo Main
+        int filas;
+        int columnas;
+
+        for (columnas = 1; columnas <= 5; columnas++) {
+
+            for (filas = 1; filas <= 5; filas++) {
+
+                System.out.print("██");
+                System.out.print("  ");
+            }
+
+            System.out.println("");
+            for (filas = 1; filas <= 5; filas++) {
+
+                System.out.print("  ");
+                System.out.print("██");
+            }
+            System.out.println("");
+
+        }
+
+    }// Fin del Main
+}// Fin de la clase
